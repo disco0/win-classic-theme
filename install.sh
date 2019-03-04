f7bee6b56b7fc19cb84ede640bd1b877254729fd
disco0 <disco0@users.noreply.github.com>
2019-03-04 03:07:56 -0800

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
-# setopt xtrace
+
 print_err(){ print -P "%F{red}$@%f"  }
 
 # Exit if no npm
@@ -7,13 +7,13 @@ print_err(){ print -P "%F{red}$@%f"  }
 
 # Create packages dir
 local package_path="${0:a:h}/packages"
-if [[ -d "$package_path/" ]] \
+if [[ ! -d "$package_path/" ]] \
 then
     mkdir -p $package_path || { print_err "unable to find/create packages directory [ %U$package_path%u ]" && return 1 }
 fi
 
 # Check for and install vsce if needed
-npm list -g | grep vsce *>/dev/null \
+npm list -g | grep vsce >/dev/null 2>&1 \
     || npm install -g vsce --no-shrinkwrap \
     || { print -P "Try %Bsudo npm install -g vsce%b? (%By%b/n)"
          read ans
@@ -24,11 +24,11 @@ npm list -g | grep vsce *>/dev/null \
        }
 
 # Generate package
-if vsce package -o $package_path/
+if vsce package
 then
-    mv *.vsix(om[1])
+    ls ${0:a:h}
+    mv *.vsix(om[1]) $package_path/
 else
     print_err "Error building package."
 fi
 
-# unsetopt xtrace
